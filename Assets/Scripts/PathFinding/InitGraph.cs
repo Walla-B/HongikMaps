bba2544b05e6251c3a10c09ed782569c41311daa
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-07-08 23:35:56 +0900

    Start, End node Initialization by raycast

--- Assets/Scripts/PathFinding/InitGraph.cs
+++ Assets/Scripts/PathFinding/InitGraph.cs
@@ -6,6 +6,7 @@ using System.IO;
 public class InitGraph : MonoBehaviour
 {
     // Start is called before the first frame update
+    private Graph graphcomp;
     void Awake()
     {
         Graph graph = new Graph();
@@ -69,15 +70,17 @@ public class InitGraph : MonoBehaviour
         }
 
 
-        int sttnodeindex = 0, tgtnodeindex = 30;
         
         DebugGraph.DrawGraph(graph);
+
+
+        ////this part of code should be not in Awake()
+        /*
         Dijkstra pathfinding = new Dijkstra();
 
         List<Node> path = pathfinding.Dijkstrasolve(graph,sttnodeindex,tgtnodeindex);
-        
         VisualizePathResult.VisulaizePath(path,nodeobject,edgeobject);
-
+        
 
 
         //Draw pathfinding result in scene view node with green line
@@ -93,9 +96,56 @@ public class InitGraph : MonoBehaviour
                 
  
         }
+        */
+
+        graphcomp = graph;
 
     }
+    void Update(){
+        if (calculate == true && Input.GetMouseButtonDown(0)) {
+            if (nodeselectindex < 2) {
+                myray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                Physics.Raycast(myray,out hitray);
+                Debug.DrawRay(myray.origin,myray.direction,Color.red,100f);
 
+                string tempname = hitray.collider.gameObject.name;
+                string search1 = "(";
+                string search2 = ")";
+
+                int index1 = tempname.IndexOf(search1);
+                int index2 = tempname.IndexOf(search2);
+
+
+                int nodeindex = int.Parse(tempname.Substring(index1 + 1,index2 - index1 - 1) );
+
+                if (nodeselectindex == 0) {
+                    sttnodeindex = nodeindex;
+                }
+                else if (nodeselectindex == 1) {
+                    tgtnodeindex = nodeindex;
+                }
+                nodeselectindex++;
+            }
+            else if (nodeselectindex == 2) {
+                nodeselectindex = 0;
+                Dijkstra pathfind = new Dijkstra();
+                visualize.VisulaizePath(pathfind.Dijkstrasolve(graphcomp,sttnodeindex,tgtnodeindex), nodeobject,edgeobject);
+
+                calculate = false;
+            }
+            //Debug.Log("called");
+        }
+    }
+    VisualizePathResult visualize = new VisualizePathResult();
+
+    private Ray myray;
+    private RaycastHit hitray;
+    [SerializeField]
+    private bool calculate = false;
+    [SerializeField]
+    private int nodeselectindex = 0;
+    [SerializeField] 
+    int sttnodeindex = 0, tgtnodeindex = 30;
     [SerializeField]
     private GameObject nodeobject;
     [SerializeField]
--- Assets/Scripts/PathFinding/VisualizePathResult.cs
+++ Assets/Scripts/PathFinding/VisualizePathResult.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class VisualizePathResult
+public class VisualizePathResult
 {
     //using linerenderer component to draw path is easy, but looks bad.
     //calculating each vertex at certain nodes and making a mesh to render
     //a component might be hard, but i will look good with a Unlit/Color shader.
-    public static void VisulaizePath (List<Node> node , GameObject nodeobject, GameObject edgeobject) {
+
+    private List<GameObject> pathcomponent = new List<GameObject>();
+    public void VisulaizePath (List<Node> node , GameObject nodeobject, GameObject edgeobject) {
         //Quaternion quaternion = new Quaternion();
+
+        //Destroys Previous 
+        OncallDestroyPrevious();
         Vector3 anglebetweennode = new Vector3();
         Vector3 lengthtoscale = new Vector3();
         float lengthbetweennode = new float();
         //drawing nodes 
         for (int i = 0 ; i < node.Count; i++ ) {
-            GameObject.Instantiate(nodeobject,node[i].Coordinate,Quaternion.identity);
-            
+            GameObject nodecompobject = GameObject.Instantiate(nodeobject,node[i].Coordinate,Quaternion.identity);
+            pathcomponent.Add(nodecompobject);
+
             if (i < node.Count - 1) {
                 //draw line between node[i].Coordinate, node[i+1].Coordinate
 
@@ -27,8 +33,9 @@ public static class VisualizePathResult
                 //quaternion.eulerAngles = Vector3.up
                 
                 
-                GameObject thisobject = GameObject.Instantiate(edgeobject,(node[i].Coordinate + node[i+1].Coordinate)/2,Quaternion.LookRotation(anglebetweennode,Vector3.up)); //.transform.localScale = lengthtoscale;
-                thisobject.transform.localScale = lengthtoscale;
+                GameObject edgecompobject = GameObject.Instantiate(edgeobject,(node[i].Coordinate + node[i+1].Coordinate)/2,Quaternion.LookRotation(anglebetweennode,Vector3.up)); //.transform.localScale = lengthtoscale;
+                edgecompobject.transform.localScale = lengthtoscale;
+                pathcomponent.Add(nodecompobject);
             }
             else if (i == node.Count -1) {
                 break;
@@ -37,6 +44,13 @@ public static class VisualizePathResult
         }    
         //drawing edges
 
+    }
+    public void OncallDestroyPrevious(){
+        Debug.Log("count : " + pathcomponent.Count);
+        for (int i = 0; i < pathcomponent.Count; i++) {
+            GameObject.Destroy(pathcomponent[i]);
+            Debug.Log("destroyed" + i);
+        }
     }
 
 }
