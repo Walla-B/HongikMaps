a30f0b675a4e4de2d2064edfccc41e6bae02eb48
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-07-26 18:47:46 +0900

    Esc key now expected to work accordingly with UI hierarchy status

---
+++ Assets/Scripts/UI/InvokeEvent.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class InvokeEvent : MonoBehaviour
+{
+    [System.Serializable]
+    public class MyEventType : UnityEvent { }
+    public MyEventType OnEvent;
+
+    public void InvokeEvents() {
+        OnEvent.Invoke();
+    }
+}
--- Assets/Scripts/UI/UIController.cs
+++ Assets/Scripts/UI/UIController.cs
@@ -1,14 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+
+using TMPro;
 
 public class UIController : MonoBehaviour
 {
+
+    // if uiDepth is zero, it means canvas status is in map mode.
+    // if one, status can be in searchmode, sidepanelmode, information mode.
+    // if two, status can be layer under the uiDepth 1
+    [SerializeField]
+    private int uiDepth = 0;
     private int escapecount = 0;
     [SerializeField]
     private GameObject quitnotification , uicanvas;
-   void Update()
+
+    [System.Serializable]
+    public class MyEventType : UnityEvent { }
+
+    public MyEventType D1Event, D2Event_example;
+
+
+    [SerializeField]
+    private TextMeshProUGUI mode;
+    void Update()
     {
+        mode.text = uiDepth.ToString();
         /*
         for testing in Playmode
 
@@ -23,10 +42,27 @@ public class UIController : MonoBehaviour
 
             StartCoroutine(ClickTime());
         }
+        
         */
 
         //If platform it Android and Escape button is pressed
        if (Application.platform == RuntimePlatform.Android) {
+
+           if (uiDepth == 0 && Input.GetKeyDown(KeyCode.Escape)) {
+               escapecount++;
+
+               StartCoroutine(ClickTime());
+               if (escapecount > 1) {
+                   Application.Quit();
+               }
+                GameObject notification = Instantiate(quitnotification,new Vector3(Screen.width/2,Screen.height/12,0f),Quaternion.identity,uicanvas.transform);
+                GameObject.Destroy(notification,4f);
+           }
+           else if (uiDepth == 1 && Input.GetKeyDown(KeyCode.Escape)){
+               D1Event.Invoke();
+               DecreaseUIDepth();
+           }
+           /*
            if (Input.GetKeyDown(KeyCode.Escape)){
                 escapecount++;
 
@@ -39,6 +75,7 @@ public class UIController : MonoBehaviour
                 GameObject notification = Instantiate(quitnotification,new Vector3(Screen.width/2,Screen.height/12,0f),Quaternion.identity,uicanvas.transform);
                 GameObject.Destroy(notification,4f);
            }
+           */
 
        }
        else if (Application.platform == RuntimePlatform.IPhonePlayer) {
@@ -48,6 +85,25 @@ public class UIController : MonoBehaviour
            }
        }
     }
+
+    // pretty sure there is a better way of doing this
+    public void UIDepthtoOne(){
+        uiDepth = 1;
+    }
+    public void UIDepthtoTwo(){
+        uiDepth = 2;
+    }
+    
+    public void IncreaseUIDepth(){
+        uiDepth++;
+    }
+
+    public void DecreaseUIDepth(){
+        if (uiDepth >= 1) {
+            uiDepth--;
+        }
+    }
+
     private IEnumerator ClickTime() {
         yield return new WaitForSeconds(4f);
         escapecount = 0;
