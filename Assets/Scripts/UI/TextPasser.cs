81609be11a6e9c35ebda3c37c6824b48482ba3f8
Walla-B <81977508+Walla-B@users.noreply.github.com>
2021-08-14 23:39:18 +0900

    Added TextPasser.cs
    TextPasser.cs sends text from one TMP_InputField to another InputField
    In Scene : Button_SetToTarget now sets InputField_Path_Target.text with InputField_Target.text

---
+++ Assets/Scripts/UI/TextPasser.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextPasser : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_InputField from, to;
+
+    public void PassText() {
+        to.text = from.text;
+    }
+}
